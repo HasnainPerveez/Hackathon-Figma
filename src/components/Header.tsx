d6eed766e542db768638b30619fcc4c339f558df
Hasnain Perveez <hasnainansari22441@gmail.com>
2024-12-10 04:28:13 +0500

    hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,8 +12,8 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className='h-full 2xl:h-[950px]  relative bg-[url("/images/restaurant/Hero/unsplash_qom5MPOER-I.svg")] bg-cover bg-center'>
-      <div className="absolute inset-0 bg-[rgba(13,13,13,0.95)]"></div>
+    <div className='h-full lg:h-[950px]  relative bg-[url("/images/restaurant/Hero/unsplash_qom5MPOER-I.svg")] bg-cover bg-center'>
+      <div className="absolute h-full inset-0 bg-[rgba(13,13,13,0.95)]"></div>
     <div className="relative z-10 w-full h-[87px] pt-[45px] md:px-[50px] lg:px-[100px]  xl:px-[200px] 2xl:px-[300px] justify-center items-center">
       <div className="flex justify-center items-center text-2xl font-bold">
         <span className="text-[#ff9f0d]">Food</span>
@@ -54,7 +54,7 @@ export default function Header() {
         <div className="w-full justify-center flex items-center md:flex md:w-auto ">
           <section className="flex items-center text-center w-[310px] h-[54px] p-4 border border-[#FF9F0D] rounded-[27px]">
             <input
-              className="w-full h-full bg-transparent border-none "
+              className="w-full h-full bg-transparent border-none px-8 py-6 rounded-full "
               type="text"
               placeholder="Search..."
             />
@@ -65,14 +65,14 @@ export default function Header() {
               <IoSearch className="w-[24px] h-[24px]" />
             </Button>
           </section>
-          <PiHandbag className="flex w-[24px] h-[24px] ml-3" />
+          <PiHandbag className="flex w-[24px] h-[24px] ml-3 hover:text-[#FF9F0D] cursor-pointer" />
         </div>
       </nav>
     </div>
-    <section className="flex h-full w-full mt-20 overflow-scroll">
-      <div className='relative z-10 w-full flex flex-col lg:flex-row lg:mx-[100px] xl:mx-[248px] lg:justify-between'>
-        <div className="flex lg:w-1/2">
-          <div className="hidden xl:pt-[90px] md:h-[492px] md:w-[25.28] xl:flex flex-row lg:flex-col justify items-center xl:gap-7">
+    <section className="flex mt-20 overflow-scroll">
+      <div className='relative z-10 w-full h-full flex flex-col lg:flex-row lg:justify-between lg:items-center lg:mx-[100px] 2xl:mx-[248px]'>
+      <div className='lg:flex'>
+          <div className="hidden md:h-[492px] md:w-[25.28] xl:flex flex-row lg:flex-col justify items-center xl:gap-7">
             {/* <hr className="mx-9 w-[158px] h-3 bg-[rgba(255, 255, 255, 1)] rotation-0 md:rotate-90"/> */}
             <div className="w-px h-[158px] bg-white space-y-2"></div>
             <Link href="#" className="social-icon">
@@ -87,7 +87,7 @@ export default function Header() {
             {/* <hr className="mx-9 w-[158px] h-3 bg-[rgba(255, 255, 255, 1)] rotation-0 md:rotate-90"/> */}
             <div className="w-px h-[158px] bg-white space-y-2"></div>
           </div>
-          <div className="ml-10 mr-10 xl:ml-20 xl:pt-[120px] items-center justify-center text-center md:text-left space-y-5">
+          <div className="mx-6 xl:ml-20 items-center justify-center text-center md:text-left space-y-5">
             <p className="w-full lg:w-[472px] text-[32px] leading-10 text-[#FF9F0D]">
               Its Quick & Amusing!
             </p>
@@ -100,13 +100,13 @@ export default function Header() {
             </p>
             <Button
               variant="outline"
-              className="w-[190px] h-[60px] rounded-[30px] bg-[#FF9F0D] font-normal"
+              className="w-[190px] h-[60px] rounded-[30px] bg-[#FF9F0D] hover:text-[#FF9F0D] font-normal"
             >
               See Menu
             </Button>
           </div>
-        </div>
-        <div className="flex h-auto lg:w-1/2 justify-center items-center">
+          </div>
+        <div className="lg:flex">
           <Image 
             className="w-full object-contain"
             src="/images/restaurant/Hero/Image.svg"
